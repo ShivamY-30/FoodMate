8804dc6185cf38d6c2c2e7d4670ee5329b08f303
Yadav Shivam Prabhubhai <yadavshivamp90671@gmail.com>
2024-06-25 18:40:56 +0530

    Eight Commit

--- Frontend/src/Store/StoreContextProvider.jsx
+++ Frontend/src/Store/StoreContextProvider.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:8000/"
+    const url = "https://foodmate-f45t.onrender.com/"
     const [cartobj , setcartobj] = useState({});
     // const [token,setToken] = useState("");
     // const [food_list,setFoodList] = useState([])
